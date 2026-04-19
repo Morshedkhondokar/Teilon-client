b9ad6bcff3c4cf036e204141753e55f2cb6cb007
Morshed Khondokar <morshedkhondokar99@.com>
2026-04-19 13:50:45 +0600

    fix: scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/nav/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <div className="max-w-7xl mx-auto">
+      {/* Scroll to Top */}
+      <ScrollToTop />
       {/* navbar */}
       <Navbar />
       {/* outlet */}
